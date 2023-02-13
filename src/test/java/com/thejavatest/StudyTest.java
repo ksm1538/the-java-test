d26ca0aa53391cb403acebe5add91323494bf947
R-SMKIM-S <ksm1538@naver.com>
2023-02-13 17:58:50 +0900

    Tag, 테스트 반복 공부

--- src/test/java/com/thejavatest/StudyTest.java
+++ src/test/java/com/thejavatest/StudyTest.java
@@ -1,36 +1,57 @@
 package com.thejavatest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 // 클래스 안에 있는 모든 @Test에 대해 일괄적으로 적용. (아래는 언더바를 공백으로 치환하는 규칙)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 	@Test
 	@DisplayName("테스트")		// 해당 테스트의 이름을 변경
+	@Tag("태그")
 	void test() {
 		System.out.println("TEST");
 	}
 
-	@Test		// 실제 Test
+	@Test		
+	@customTag
 	void create_test() {
 		Study study = new Study();
 		assertNotNull(study);
 		System.out.println("createTest");
 	}
 	
-	@Test		
-	void create_11() {
+	@RepeatedTest(value = 5, name ="{displayName}, {currentRepetition} of {totalRepetitions}")
+	@DisplayName("반복 테스트")
+	void repeat_test() {
 		System.out.println("create1");
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"1번", "2번", "3번"})
+	@DisplayName("파라미터 반복 테스트")
+	void repeat_param_test(String s) {
+		System.out.println(s);
+	}
+	
+	@ParameterizedTest
+	@CsvSource({"1, '1번'", "1, '2번'"})
+	@DisplayName("파라미터 반복 테스트_csv")
+	void repeat_param_test2(String s) {
+		System.out.println(s);
 	}
 	
 	// 이 어노테이션은 반드시 static 사용해야함
